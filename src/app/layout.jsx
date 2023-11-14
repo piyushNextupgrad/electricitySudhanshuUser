961fb5417ebf513fc6b538acf62f0c76d600966d
pradeepuiux <148767049+pradeepuiux@users.noreply.github.com>
2023-11-14 03:40:44 -0800

    Update layout.jsx
    layout changes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,10 @@
 import { Inter } from 'next/font/google'
 import "../styles/admin.css"
 import './globals.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Script from 'next/script';
+
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,6 +19,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></Script>
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></Script>
       </body>
     </html>
   )
