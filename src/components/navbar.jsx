208b25a88725c4808123f6752a3415be906f01e9
pradeepuiux <148767049+pradeepuiux@users.noreply.github.com>
2023-11-20 04:06:00 -0800

    Update navbar.jsx
    search section add class

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -10,15 +10,15 @@ const Navbar = () => {
             <Link href="/"><Image src="/services/logo.png" height={50} width={200} alt="img" /></Link>
         </div>
         <div className={'{style.section1_NavSearch} col-xxl-9 col-xl-9 col-lg-9'} >
-            <div className={style.section1_NavSearchbox}>
+            <div className={'{style.section1_NavSearchbox} col-xxl-5 col-xl-5 col-lg-5'} >
                 <input type="search" placeholder="Current Location" />
                 <MdLocationPin />
             </div>
-            <div className={style.section1_NavSearchbox}>
+            <div className={'{style.section1_NavSearchbox} col-xxl-5 col-xl-5 col-lg-5'} >
                 <input type="search" placeholder="Search for services" />
                 <FaSearch />
             </div>
-            <div className={style.section1_NavIcons}>
+            <div className={'{style.section1_NavIcons} col-xxl-2 col-xl-2 col-lg-2'} >
                 <FaShoppingCart />
                 <FaUser />
                 <FaBars />
