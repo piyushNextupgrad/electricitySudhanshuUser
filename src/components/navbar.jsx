a8a977fd8e0714847c0a049d23b5a3b0758e381b
pradeepuiux <148767049+pradeepuiux@users.noreply.github.com>
2023-11-20 04:00:56 -0800

    Update navbar.jsx
    navbar inner dynamic

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,10 +6,10 @@ import { FaSearch, FaUser, FaBars, FaRegWindowClose, FaFacebookF, FaGooglePlusG,
 
 const Navbar = () => {
     return (<nav className={style.navbar}>
-        <div className={style.section1_NavLogo}>
+        <div className={'${style.section1_NavLogo} col-xxl-3 col-xl-3 col-lg-3 col-md-6 col-4'}>
             <Link href="/"><Image src="/services/logo.png" height={50} width={200} alt="img" /></Link>
         </div>
-        <div className={style.section1_NavSearch}>
+        <div className={'{style.section1_NavSearch} col-xxl-9 col-xl-9 col-lg-9'} >
             <div className={style.section1_NavSearchbox}>
                 <input type="search" placeholder="Current Location" />
                 <MdLocationPin />
