3895eea4ebf3f94b8e960c4db12a265d8a7a1ca5
pradeepuiux <148767049+pradeepuiux@users.noreply.github.com>
2023-11-20 04:13:49 -0800

    Update navbar.jsx
    home page header and location and search section added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,28 +5,47 @@ import { MdLocationPin } from "react-icons/md";
 import { FaSearch, FaUser, FaBars, FaRegWindowClose, FaFacebookF, FaGooglePlusG, FaSkype, FaShoppingCart, FaTwitter, FaChevronRight } from "react-icons/fa"
 
 const Navbar = () => {
-    return (<nav className={style.navbar}>
-        <div className={'${style.section1_NavLogo} col-xxl-3 col-xl-3 col-lg-3 col-md-6 col-4'}>
-            <Link href="/"><Image src="/services/logo.png" height={50} width={200} alt="img" /></Link>
-        </div>
-        <div className={'{style.section1_NavSearch} col-xxl-9 col-xl-9 col-lg-9'} >
-            <div className="row">
-            <div className={'{style.section1_NavSearchbox} col-xxl-5 col-xl-5 col-lg-5'} >
-                <input type="search" placeholder="Current Location" />
-                <MdLocationPin />
-            </div>
-            <div className={'{style.section1_NavSearchbox} col-xxl-5 col-xl-5 col-lg-5'} >
-                <input type="search" placeholder="Search for services" />
-                <FaSearch />
-            </div>
-            <div className={'{style.section1_NavIcons} col-xxl-2 col-xl-2 col-lg-2'} >
-                <FaShoppingCart />
-                <FaUser />
-                <FaBars />
-            </div>
-                 </div>
-        </div>
-    </nav>);
+    return ( <nav className={style.nav}>
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-xxl-3 col-xl-3 col-lg-3 col-md-6 col-4">
+                {" "}
+                <Link href="#">
+                  <Image
+                    src="/homepage/logo.png"
+                    height={50}
+                    width={200}
+                    className={style.logo}
+                    alt="logo"
+                  />
+                </Link>
+              </div>
+              <div className={`${style.nav_icon} col-xxl-9 col-xl-9 col-lg-9`}>
+				   <div className={style.banner_input}>
+                    <div className={style.banner_input1}>
+                      {" "}
+                      <MdLocationPin />
+                      <input type="text" placeholder="Current Locaction" />
+                    </div>
+                    <div className={style.banner_input2}>
+                      {" "}
+                      <AiOutlineSearch />
+                      <input type="text" placeholder="Search for Services" />
+                    </div>
+                  </div>
+                <Link href="#">
+                  <FaShoppingCart />
+                </Link>
+                <Link href="#">
+                  <FaUser />
+                </Link>
+                <Link href="#" onClick={SidemenuAction}>
+                  <FaBars onClick={SidemenuAction} />
+                </Link>
+              </div>{" "}
+            </div>{" "}
+          </div>
+        </nav>);
 }
 
 export default Navbar;
